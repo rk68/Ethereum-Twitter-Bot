72c1ead4bde7d6047d6731e60c7d48cb7268d8f6
rk68 <71785159+rk68@users.noreply.github.com>
2020-12-31 13:17:26 +0000

    Add files via upload

---
+++ EtherBot.py
@@ -0,0 +1,52 @@
+# Import libraries
+import tweepy
+import requests
+import json
+import time
+import api_data
+
+
+# Authentication
+auth_handler = tweepy.OAuthHandler(consumer_key=API_KEY, consumer_secret=SECRET_KEY)
+auth_handler.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# To avoid spamming
+api = tweepy.API(auth_handler, wait_on_rate_limit=True)
+
+print("Logged In..")
+
+def tweet_eth_price():
+
+    # Get Ethereum Price from coingecko
+    ethprice = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true')
+    price = ethprice.json()
+
+    # Current price
+    ethusd = price['ethereum']['usd']
+
+    # Market cap
+    eth_mc = price['ethereum']['usd_market_cap']
+
+    # 24 Hour volume
+    eth_24h_vol = price['ethereum']['usd_24h_vol']
+
+    # 24 Hour price change
+    eth_24h_change = price['ethereum']['usd_24h_change']
+
+    # Tweet message
+    message = ' ETH Price:        $'+ str(ethusd) +' USD' '\n ETH Market Cap:   $'+ str(round(eth_mc, 2)) + 'USD' '\n ETH 24hr Volume:  $' + str(round(eth_24h_vol, 2)) + 'USD' '\n ETH 24hr Change:  $' + str(round(eth_24h_change,3)) + 'USD'
+    print(message)
+
+    # Tweet the message
+    api.update_status(message)
+
+    print("Tweet posted successfully!")
+
+    # Sleep for 3 hours
+    time.sleep(3*60*60)
+
+    return print(message)
+
+# Let the bot run forever
+while True:
+    tweet_eth_price()
---
+++ api_data.py
@@ -0,0 +1,6 @@
+# API data for twitter account
+
+API_KEY             = "xyz"
+SECRET_KEY          = "xyz"
+ACCESS_TOKEN        = "xyz"
+ACCESS_TOKEN_SECRET = "xyz"
